dd07ed03b5ba46e29e9ee7538279e6adfc1e4074
Tulsonic <tulgar.dinc@gmail.com>
2020-02-04 11:19:38 +0300

    Still fixing wallrun

---
+++ Advanced Movement Game/Assets/Scripts/buttons/ButtonParrentScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonParrentScript : MonoBehaviour
+{
+    public void LoadStart()
+    {
+        SceneManager.LoadScene("Test Scene");
+    }
+
+    public void LoadOptions()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
