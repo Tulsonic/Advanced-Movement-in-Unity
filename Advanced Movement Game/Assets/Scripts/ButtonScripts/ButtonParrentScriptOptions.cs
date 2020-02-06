bf44948ea7e9b7348d89625102ef8ac9df1c2d27
Tulsonic <tulgar.dinc@gmail.com>
2020-02-06 18:27:45 +0300

    Options done

--- Advanced Movement Game/Assets/Scripts/ButtonScripts/ButtonParrentScriptOptions.cs
+++ Advanced Movement Game/Assets/Scripts/ButtonScripts/ButtonParrentScriptOptions.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ButtonParrentScriptOptions : MonoBehaviour
 {
 
     private GameManager gameManager;
+    private Slider slider;
+    private InputField input;
 
     private void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        slider = GameObject.Find("Slider").GetComponent<Slider>();
+        slider.value = gameManager.newSensitivity;
+        input = GameObject.Find("InputField").GetComponent<InputField>();
     }
 
     public void GoBack()
@@ -21,5 +27,14 @@ public class ButtonParrentScriptOptions : MonoBehaviour
     public void GetSliderValue(float value)
     {
         gameManager.newSensitivity = value;
+    }
+
+    public void GetInputValue(string text) {
+        slider.value = int.Parse(text);
+    }
+
+    public void UpdateInputValue(float value)
+    {
+        input.text = value.ToString();
     }
 }
